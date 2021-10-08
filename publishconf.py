42ea667154615fc67e1fe93de75aadbaae548657
Ayo Akindele <a.akindele01@gmail.com>
2021-10-08 13:07:12 -0600

    made changes

--- publishconf.py
+++ publishconf.py
@@ -14,7 +14,7 @@ SITEURL = 'https://aakindel.github.io/404_lab_5'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
-CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
+# CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
 
 DELETE_OUTPUT_DIRECTORY = True
 OUTPUT_PATH = "docs/"
